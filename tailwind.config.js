165acee5912a94e5c4ab60609a54e049f20cb8a4
Gonzalo Fernández Suárez <83967769+gonfdez@users.noreply.github.com>
2025-08-05 10:45:30 +0000

    Customize Tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        'auto-fill-200': 'repeat(auto-fill, minmax(200px, 1fr))',
+        'auto-fill-250': 'repeat(auto-fill, minmax(250px, 1fr))',
+      },
+      aspectRatio: {
+        'game': '4/3',
+      }
+    },
+  },
+  plugins: [],
+}
